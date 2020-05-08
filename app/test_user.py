f03807641ffd014e17f38509e7b1f265d9e3f640
lorrainekamanda <lorrainekamanda@gmail.com>
2020-05-08 16:44:58 +0300

    update tests

--- app/test_user.py
+++ app/test_user.py
@@ -12,7 +12,13 @@ class UserTest(unittest.TestCase):
         self.assertTrue(self.new_user.pass_secure is not None)
 
     def setUp(self):
-        self.user_James = User(username = 'James',password = 'potato', email = 'james@ms.com')
+        self.user_lorraine = User(username = 'lorraine',password = 'lorra', email = 'lorra@.gmailom')
         self.new_pitch= Role(role_id=1,role_title='Insiprational',image_path="https://image.tmdb.org/t/p/w500/jdjdjdjn",user = self.user_James )
-
+    
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_review.role_id,1)
+        self.assertEquals(self.new_review.role_title,'Inspirational')
+        self.assertEquals(self.new_review.image_path,"https://image.tmdb.org/t/p/w500/jdjdjdjn")
+        self.assertEquals(self.new_review.movie_review,'This movie is the best thing since sliced bread')
+        self.assertEquals(self.new_review.user,self.user_lorraine)
 
