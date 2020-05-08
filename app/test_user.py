301b1bcd4823a99d0085b591311957b2318dd1b4
lorrainekamanda <lorrainekamanda@gmail.com>
2020-05-08 16:39:28 +0300

    add more tests

--- app/test_user.py
+++ app/test_user.py
@@ -1,6 +1,6 @@
 import unittest
 
-from model import User
+from model import User,Role,Comment
 
 class UserTest(unittest.TestCase):
 
@@ -10,4 +10,9 @@ class UserTest(unittest.TestCase):
 
     def test__passoword_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
+
+    def setUp(self):
+        self.user_James = User(username = 'James',password = 'potato', email = 'james@ms.com')
+        self.new_pitch= Role(role_id=1,role_title='Insiprational',image_path="https://image.tmdb.org/t/p/w500/jdjdjdjn",user = self.user_James )
+
 
